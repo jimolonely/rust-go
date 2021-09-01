341336771a4b41113149ba4e021bade3dbfc780d
wangpeng417 <wangpeng417@jd.com>
2021-09-01 11:25:16 +0800

    addclose

---
+++ close01/src/main.rs
@@ -0,0 +1,14 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn workout(tensity: u32, num: u32) {
+    let exp = |n|{
+        println!("缓慢的计算");
+thread::sleep(Duration::from_secs(2));
+n
+    }
+}
